cf0a87adfcbabcbaa8792b2b93e93636fbacf6c5
mikula8989 <m.kauzonas@gmail.com>
2026-08-30 15:07:38 +0300

    Cache v2 workout override data

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 
-const CACHE = "workout-app-v1";
-const SHELL = ["./","./index.html","./style.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE = "workout-app-v2";
+const SHELL = ["./","./index.html","./style.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png","./program-overrides.json"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
 self.addEventListener("activate", e => e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
 ));
 self.addEventListener("fetch", e => {
   const url = new URL(e.request.url);
-  if(url.pathname.endsWith("/program.json")){
+  if(url.pathname.endsWith("/program.json") || url.pathname.endsWith("/program-overrides.json")){
     e.respondWith(fetch(e.request).then(r=>{
       const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
     }).catch(()=>caches.match(e.request)));
